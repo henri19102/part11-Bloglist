79d7ca9a619a183380526d0a6047d37e33d97d40
Henri Mattila <henri19102@student.hamk.fi>
2021-04-19 16:51:21 +0300

    pipeline e2e test try4

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -6,6 +6,5 @@ describe('Blog app', function () {
     it('You could try to login', function () {
       cy.visit('http://localhost:3003'); // eslint-disable-line
       cy.contains('login').click(); // eslint-disable-line
-      cy.contains("wrong");// eslint-disable-line
     })
   })
