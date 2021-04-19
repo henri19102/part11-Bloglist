ae38dab51c4720fe284911745c6a12e243330550
Henri Mattila <henri19102@student.hamk.fi>
2021-04-19 15:37:08 +0300

    pipeline e2e test try2

---
+++ build/precache-manifest.ca0603fad3e5a38ebaf9815851a54cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d17b079e9af1e81e84f7b05f0251778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4567d78cbebb2a6a934",
+    "url": "/static/css/main.19c6c5a2.chunk.css"
+  },
+  {
+    "revision": "d0cc5b7ce5b559d2ee66",
+    "url": "/static/js/2.d810b9c8.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.d810b9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4567d78cbebb2a6a934",
+    "url": "/static/js/main.488b6e67.chunk.js"
+  },
+  {
+    "revision": "fec8707ab4aa50d592e7",
+    "url": "/static/js/runtime-main.364314cc.js"
+  }
+]);
